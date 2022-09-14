88e6b7e206f19a28b1c7ff2dcfa4f87d3875cd48
DenisNikiti <den.hikitin@gmail.com>
2022-09-15 01:08:14 +0300

    1

--- js/script_5.js
+++ js/script_5.js
@@ -1,4 +1,17 @@
 const inputEl = document.querySelector("#name-input")
 const outputEl = document.querySelector("#name-output")
-inputEl.addEventListener("input", () => { outputEl.textContent = inputEl.value })
+inputEl.addEventListener("input", () => {
+// console.log(inputEl.value)
+    if (inputEl.value === "") {
+        outputEl.textContent = "Anonymous"
+    }
+    else {
+        outputEl.textContent = inputEl.value
+        =
+    }
+    
+
+
+
+})
 
